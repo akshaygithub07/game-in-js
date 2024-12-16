44a6d176c631cb18a4d956eab883020b0fe4927d
Fire <akshaysunilkumar2004@gmail.com>
2024-12-17 00:29:38 +0530

    started making the spin function

--- project.js
+++ project.js
@@ -8,6 +8,28 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT ={
+    "A":2,
+    "B":4,
+    "c":6,
+    "D":8
+}
+
+const SYMBOL_VALUES = {
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2
+
+}
+
+ 
+
+
+
 const deposit = ()=>{
     while(true){
         const depositAmount = prompt("Enter a deposit amount: ")
@@ -36,21 +58,32 @@ const getNumberOfLines=()=>{
 
 }
 
-const getBet = () =>{
+const getBet = (balance,lines) =>{
     while(true){
-        const bet = prompt("Enter amount of bet: ")
+        const bet = prompt("Enter amount of bet per line: ")
         const numberBet = parseFloat(bet);
     
-        if(isNaN(numberBet)||numberBet<=0 ||numberBet>balance){
+        if(isNaN(numberBet)||numberBet<=0 ||numberBet> balance/lines){
             console.log("Invalid bet. Try again.")
     }else{
         return numberBet;
     }
-};
-
 }
 
+};
+
+const spin = ()=> {
+    const symbols = [];
+    for(const [symbol,count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i = 0;i<count;i++){
+            symbols.push(symbol);
+        }
+    }
+    console.log(symbols);
+};
+
+spin();
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
-const bet = getBet(balance);
+const bet = getBet(balance,numberOfLines);
